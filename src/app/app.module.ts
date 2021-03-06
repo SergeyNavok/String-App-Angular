99bc4231b8083323ed22d223904cbd1cde0b17b7
SergeyNavok <poketa@tut.by>
2021-03-06 21:09:56 +0300

    feat: add line component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LineComponent } from './line/line.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FieldComponent
+    FieldComponent,
+    LineComponent
   ],
   imports: [
     BrowserModule
