5bd2cfc6f38c6e3b94d81321ffa476c0fdd57e4b
SergeyNavok <poketa@tut.by>
2021-03-06 20:13:06 +0300

    feat: add in field @component

---
+++ src/app/field/field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-field',
+  templateUrl: 'field.component.html',
+  styleUrls: ['field.component.scss']
+})
+
+
+export class fieldComponent {
+
+}
